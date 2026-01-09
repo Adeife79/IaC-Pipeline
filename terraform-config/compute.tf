3e1628002e4cd866bcfff17faf41146986c2377e
E335-comma <ifeoluwaaderoju6@gmail.com>
2026-01-09 21:45:48 +0100

    fix

--- terraform-config/compute.tf
+++ terraform-config/compute.tf
@@ -1,29 +1,29 @@
-resource "aws_iam_role" "ssm_role" {
-  name = "SSMRoleForEC2"
+#resource "aws_iam_role" "ssm_role" {
+#  name = "SSMRoleForEC2"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17"
+#   Statement = [
+#     {
+ #       Effect = "Allow",
+ #       Principal = {
+  #        Service = "ec2.amazonaws.com"
+   #     },
+    #    Action = "sts:AssumeRole"
+     # }
+    #]
+  #})
+#}
 
-resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
-  role = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
+#resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
+#  role = aws_iam_role.ssm_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+#}
 
-resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = "SSMInstanceProfile"
-  role = aws_iam_role.ssm_role.name
-}
+#resource "aws_iam_instance_profile" "ssm_instance_profile" {
+#  name = "SSMInstanceProfile"
+#  role = aws_iam_role.ssm_role.name
+#}
 
 resource "aws_instance" "web" {
   ami           = var.web_instance_ami
